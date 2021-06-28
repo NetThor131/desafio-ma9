853d65f8afa6d392ac02d53f2b3890ebdb13ae92
João Ferreira dos Santos Neto <joaont.dev@gmail.com>
2021-06-27 22:12:50 -0300

    Consome servico e adiciona providers

--- src/app/modules/features/home/home.module.ts
+++ src/app/modules/features/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home';
+import { ClientesService } from '@data/services';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [CommonModule, HomeRoutingModule],
+  providers: [ClientesService],
 })
 export class HomeModule {}
