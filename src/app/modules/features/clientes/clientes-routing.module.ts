f780618dc8ee7612e027affc19bb78ded51300a1
João Ferreira dos Santos Neto <joaont.dev@gmail.com>
2021-06-29 23:37:57 -0300

    Cria componente de visualizar

--- src/app/modules/features/clientes/clientes-routing.module.ts
+++ src/app/modules/features/clientes/clientes-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AdicionarComponent } from './pages/adicionar';
+import { VisualizarComponent } from './pages/visualizar';
 
-const routes: Routes = [{ path: 'adicionar', component: AdicionarComponent }];
+const routes: Routes = [
+  { path: 'adicionar', component: AdicionarComponent },
+  { path: 'visualizar', component: VisualizarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/features/clientes/clientes.module.ts
+++ src/app/modules/features/clientes/clientes.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientesRoutingModule } from './clientes-routing.module';
-import { AdicionarComponent } from './pages/adicionar/adicionar.component';
+import { AdicionarComponent } from './pages/adicionar';
+import { VisualizarComponent } from './pages/visualizar';
 
 @NgModule({
-  declarations: [
-    AdicionarComponent
-  ],
+  declarations: [AdicionarComponent, VisualizarComponent],
   imports: [CommonModule, ClientesRoutingModule],
 })
 export class ClientesModule {}
