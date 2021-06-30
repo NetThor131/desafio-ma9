fa4c45e25e4dca69e47671c9d005207c625f9319
João Ferreira dos Santos Neto <joaont.dev@gmail.com>
2021-06-29 22:59:25 -0300

    Cria o header da tela home

--- src/app/modules/features/home/home.module.ts
+++ src/app/modules/features/home/home.module.ts
@@ -5,9 +5,10 @@ import { ClientesService } from '@data/services';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home';
 import { TabelaComponent } from './components/tabela';
+import { HeaderHomeComponent } from './components/header-home/header-home.component';
 
 @NgModule({
-  declarations: [HomeComponent, TabelaComponent],
+  declarations: [HomeComponent, TabelaComponent, HeaderHomeComponent],
   imports: [CommonModule, HomeRoutingModule],
   providers: [ClientesService],
 })
