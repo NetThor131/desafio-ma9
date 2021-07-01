1968a77709b6363710d04756899d6c63022129c7
João Ferreira dos Santos Neto <joaont.dev@gmail.com>
2021-06-30 23:03:07 -0300

    Cria core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { UiModule } from '@modules/ui/ui.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { UiModule } from '@modules/ui/ui.module';
 import { ToastrModule } from 'ngx-toastr';
+import { CoreModule } from './core';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    CoreModule,
     AppRoutingModule,
     HttpClientModule,
     UiModule,
